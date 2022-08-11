cc1cd490d659c6f3de6e18219532bba0f92ba69a
Galih Pratama <hanamura.iost@gmail.com>
2022-08-11 21:34:48 +0700

    Init commit

---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <Tutorial />
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+  name: 'IndexPage',
+  async asyncData({ params }) {
+    const post = await axios.get(`https://api.nuxtjs.dev/posts/2`)
+    return { post }
+  },
+}
+</script>
